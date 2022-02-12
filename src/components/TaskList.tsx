1dfbeb6c55570d486d227307c938ae5925dfb6c9
Lohana Torres <lt.leopoldo@gmail.com>
2022-02-11 21:27:36 -0300

    feat: implement todos list

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -14,16 +14,54 @@ export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
+  function checkIfIdAlreadyExists(id: number) {
+    return tasks.some((task) => {
+      return task.id === id;
+    });
+  }
+
+  function generateRandomNumber() {
+    return Math.floor(Math.random() * 10000000000000);
+  }
+
+  function handleId() {
+    let id = generateRandomNumber();
+    while (checkIfIdAlreadyExists(id)) {
+      id = Math.floor(Math.random() * 10000000000000);
+    }
+    return id;
+  }
+
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (newTaskTitle.trim()) {
+      const id = handleId();
+      const task: Task = {
+        id,
+        title: newTaskTitle,
+        isComplete: false
+      };
+      setTasks([...tasks, task]);
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const task = tasks.find((task) => task.id === id);
+    if (task) {
+      task.isComplete = !task.isComplete;
+      setTasks([...tasks]);
+    }
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const task = tasks.find((task) => task.id === id);
+    if (task) {
+      const taskPosition = tasks.indexOf(task);
+      const filteredTasks = tasks.filter((task) => task.id !== id);
+      setTasks([...filteredTasks]);
+    }
   }
 
   return (
@@ -32,9 +70,9 @@ export function TaskList() {
         <h2>Minhas tasks</h2>
 
         <div className="input-group">
-          <input 
-            type="text" 
-            placeholder="Adicionar novo todo" 
+          <input
+            type="text"
+            placeholder="Adicionar novo todo"
             onChange={(e) => setNewTaskTitle(e.target.value)}
             value={newTaskTitle}
           />
@@ -50,7 +88,7 @@ export function TaskList() {
             <li key={task.id}>
               <div className={task.isComplete ? 'completed' : ''} data-testid="task" >
                 <label className="checkbox-container">
-                  <input 
+                  <input
                     type="checkbox"
                     readOnly
                     checked={task.isComplete}
@@ -66,7 +104,7 @@ export function TaskList() {
               </button>
             </li>
           ))}
-          
+
         </ul>
       </main>
     </section>
